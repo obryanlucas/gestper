24a4f3295f6bc4cbed0dc5d33c771e94b40b3b50
Laboratório 205 <lab205@fiemguba.srv>
2026-05-14 21:56:58 -0300

    auteração-server

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`API rodando na portadede ${PORT}`);
+  console.log(`API rodando na porta ${PORT}`);
 });
